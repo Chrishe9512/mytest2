4f18fc62177818591a09d66510627befe75a0213
chris <chrishe9512@sina.com>
2019-02-28 20:35:07 +0800

    test02

--- idea-git-test/src/cn/itcast/test/test.java
+++ idea-git-test/src/cn/itcast/test/test.java
@@ -3,5 +3,11 @@ package cn.itcast.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
     }
 }
